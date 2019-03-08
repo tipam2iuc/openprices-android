e9ce66fd6ef7f847fdcf905d26fa6eb2f0c2f4db
Fraich NGUEGANG <fraichbeth@gmail.com>
2019-03-08 06:23:21 +0100

    Fragments utilisateur connecte ecriture des fragments pour les page de l'utilisateur connecter... debut de l'ecriture de la classe user_connect_activity...

--- app/src/main/java/com/nguegangbeth/openprices/views/Creer_compte_userActivity.java
+++ app/src/main/java/com/nguegangbeth/openprices/views/Creer_compte_userActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nguegangbeth.openprices.R;
+import com.nguegangbeth.openprices.controllers.Contole_User;
 
 public class Creer_compte_userActivity extends AppCompatActivity {
 
@@ -15,11 +18,14 @@ public class Creer_compte_userActivity extends AppCompatActivity {
     private EditText editText_email_user;
     private EditText editText_motpasse_user;
     private EditText editText_confirmMotdepasse_user;
+    private TextView textView_error_message;
+    private Contole_User contole_user;
     private Button button_creer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creer_compte_user);
+        this.contole_user = Contole_User.getInstance(this);
         Init();
         EcouteButtonCreer();
     }
@@ -29,6 +35,7 @@ public class Creer_compte_userActivity extends AppCompatActivity {
         editText_email_user = (EditText)findViewById(R.id.editText_email_user);
         editText_motpasse_user = (EditText)findViewById(R.id.editText_motPasse_user);
         editText_nomprenom_user = (EditText)findViewById(R.id.editText_nom_prenom_user);
+        textView_error_message = (TextView) findViewById(R.id.textView_error_message);
         button_creer = (Button) findViewById(R.id.button_creer_user);
     }
 
@@ -36,8 +43,36 @@ public class Creer_compte_userActivity extends AppCompatActivity {
         button_creer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Creer_compte_userActivity.this, Interface_connexion_Activity.class);
-                startActivity(intent);
+                String motPasse = "" ;
+                String confirmMotPasse = "" ;
+                String email = "";
+                String nomPrenom = "";
+                try{
+                    motPasse = editText_motpasse_user.getText().toString();
+                    confirmMotPasse = editText_confirmMotdepasse_user.getText().toString();
+                    email = editText_email_user.getText().toString();
+                    nomPrenom = editText_nomprenom_user.getText().toString();
+
+                }catch(Exception ex){
+                    Toast.makeText(Creer_compte_userActivity.this, "Champ invalid", Toast.LENGTH_SHORT);
+                }
+
+                if(motPasse == "" || confirmMotPasse == ""|| email == ""||nomPrenom == ""){
+                    textView_error_message.setText("Un ou plusieurs champ sont invalides!");
+                }
+
+                else{
+                    /*if(motPasse != confirmMotPasse){
+                    textView_error_message.setText("Les mots de passe ne correspondent pas");
+                    }*/
+                    //else{
+                        contole_user.CreerUser(email, nomPrenom, motPasse, Creer_compte_userActivity.this);
+                        Intent intent = new Intent(Creer_compte_userActivity.this, Interface_connexion_Activity.class);
+                        startActivity(intent);
+                    //}
+
+                }
+
             }
         });
     }
--- app/src/main/java/com/nguegangbeth/openprices/views/Interface_connexion_Activity.java
+++ app/src/main/java/com/nguegangbeth/openprices/views/Interface_connexion_Activity.java
@@ -1,29 +1,40 @@
 package com.nguegangbeth.openprices.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.nguegangbeth.openprices.R;
+import com.nguegangbeth.openprices.controllers.Contole_User;
+import com.nguegangbeth.openprices.modeles.User;
 
 public class Interface_connexion_Activity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    private TextView invalid_identifier;
     private Button button_connexion;
+    private Contole_User contole_user;
 
+    private Integer authenticate;//change de valeur si l'utilisateur qui se connecte existe
+    private String Email, Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface_connexion_);
+        this.contole_user = Contole_User.getInstance(this);
         Init();
+        EcouteButtonConnexion();
     }
 
     private void Init(){
         email = (EditText)findViewById(R.id.editText_email_connect);
         password = (EditText)findViewById(R.id.editText_motpasse_connect);
+        invalid_identifier = (TextView)findViewById(R.id.textView_invalid_identifiant);
         button_connexion = (Button)findViewById(R.id.button_connexion);
     }
 
@@ -31,7 +42,25 @@ public class Interface_connexion_Activity extends AppCompatActivity {
         button_connexion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try{
+                    Email = email.getText().toString();
+                    Password = password.getText().toString();
+                    for(User user:contole_user.getLesUsers()){
+                        if(Email.equals(user.getEMAIL()) == true && Password.equals(user.getMotPasse())){
+                            authenticate=1;
+                        }
+                    }
 
+                    if(authenticate == 1){
+                        Intent intent = new Intent(Interface_connexion_Activity.this, User_connectActivity.class);
+                        startActivity(intent);
+                    }else {
+                            invalid_identifier.setText("Identifiant invalid");
+                    }
+
+                }catch (Exception ex){
+                    ex.getMessage();
+                }
             }
         });
     }
