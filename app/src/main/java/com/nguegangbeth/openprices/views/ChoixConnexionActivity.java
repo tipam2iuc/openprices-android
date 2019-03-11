ea6efefc6aa4c80d7f79a0b9d3e16957664d862d
nomilk <loysandjo@gmail.com>
2019-03-11 18:01:29 +0100

    Special Updates about the Interface of the app

--- app/src/main/java/com/nguegangbeth/openprices/views/ChoixConnexionActivity.java
+++ app/src/main/java/com/nguegangbeth/openprices/views/ChoixConnexionActivity.java
@@ -16,49 +16,37 @@ import com.nguegangbeth.openprices.R;
 public class ChoixConnexionActivity extends AppCompatActivity {
 
     private Button button_facebook;
-    private Button button_linkedin;
     private Button button_google;
-    private Button button_inscription;
-    private Button button_connexion;
     private CheckBox checkBox_terme;
     private TextView textView_passer;
-    private TextView textView_termes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_connexion);
         Init();
-        button_inscription.setEnabled(false);
+
         button_google.setEnabled(false);
-        button_linkedin.setEnabled(false);
+
         button_facebook.setEnabled(false);
-        button_connexion.setEnabled(false);
+
         textView_passer.setEnabled(false);
 
-        EcouteButtonInscription();
-        EcouteButtonConnexion();
         /**
          * Valide les boutons du formulaire que lorsaque l'utilisateur accepte les termes du contrat
          */
         checkBox_terme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked == true){
+                if(isChecked){
                     textView_passer.setTextColor(getResources().getColor(R.color.violetFonce));
                     textView_passer.setEnabled(true);
-                    button_inscription.setEnabled(true);
                     button_google.setEnabled(true);
-                    button_linkedin.setEnabled(true);
                     button_facebook.setEnabled(true);
-                    button_connexion.setEnabled(true);
                 }else{
                     textView_passer.setTextColor(getResources().getColor(R.color.darkGray));
                     textView_passer.setEnabled(false);
-                    button_inscription.setEnabled(false);
                     button_google.setEnabled(false);
-                    button_linkedin.setEnabled(false);
                     button_facebook.setEnabled(false);
-                    button_connexion.setEnabled(false);
                 }
 
             }
@@ -72,14 +60,10 @@ public class ChoixConnexionActivity extends AppCompatActivity {
      * Jointure des boutons a mes membres
      */
     private void Init(){
-        button_connexion = (Button)findViewById(R.id.button_connexion);
         button_facebook = (Button)findViewById(R.id.button_facebook);
-        button_linkedin = (Button)findViewById(R.id.button_linkedin);
         button_google = (Button)findViewById(R.id.button_google);
-        button_inscription = (Button)findViewById(R.id.button_inscription);
         checkBox_terme = (CheckBox)findViewById(R.id.checkBox_terme);
         textView_passer = (TextView) findViewById(R.id.textView_passer);
-        textView_termes = (TextView) findViewById(R.id.textView_termes);
     }
 
     /**
@@ -94,23 +78,4 @@ public class ChoixConnexionActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void EcouteButtonInscription(){
-        button_inscription.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(ChoixConnexionActivity.this, QuestionActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void EcouteButtonConnexion(){
-        button_connexion.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(ChoixConnexionActivity.this, Interface_connexion_Activity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
 }
