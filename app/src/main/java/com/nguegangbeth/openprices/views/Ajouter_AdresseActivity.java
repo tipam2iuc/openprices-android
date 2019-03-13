8c4e532cf6ed73cf9d4a6dbc57f5f247d8e7c0e0
Fraich NGUEGANG <fraichbeth@gmail.com>
2019-03-13 10:10:02 +0100

    ajout de drawable

---
+++ app/src/main/java/com/nguegangbeth/openprices/views/Ajouter_AdresseActivity.java
@@ -0,0 +1,15 @@
+package com.nguegangbeth.openprices.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nguegangbeth.openprices.R;
+
+public class Ajouter_AdresseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter__adresse);
+    }
+}
